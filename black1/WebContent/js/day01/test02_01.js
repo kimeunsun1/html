f6a4016856bf4b5d67bc79e56fd44b26d8ffaf6f
user <user@DESKTOP-HMMHVRI>
2022-04-26 18:02:22 +0900

    0426

---
+++ black1/WebContent/js/day01/test02_01.js
@@ -0,0 +1,3 @@
+document.write('test02_01.js파일');
+document.write('<div style="color: blue; font-size: 20pt;">첫번째 외부자바스크립트 파일</div>');
+document.write('script type="text/javascript" src="/black1/resources/js/day01/test02_02.js"></script>');
---
+++ black1/WebContent/js/day01/test02_02.js
@@ -0,0 +1,3 @@
+document.write('test02_02.js');
+document.write('<div style="color: green; font-size: 16pt;">첫번째 외부자바스크립트 파일</div>');
+document.write('Nested Script File!');
